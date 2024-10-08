4f66f94d43d581e562137fd79ad049d2221691b4
panda-codes <dedeomatse@gmail.com>
2024-10-08 10:07:33 +0100

    updated build

--- js/Script.js
+++ js/Script.js
@@ -2,7 +2,20 @@ const btn = document.getElementById('menu-btn')
 const nav = document.getElementById('menu');
 
 btn.addEventListener('click',()=>{
-    btn.classList.toggle('open')
-    nav.classList.toggle('flex')
-    nav.classList.toggle('hidden')
+    // btn.classList.toggle('open')
+    // nav.classList.toggle('flex')
+    // nav.classList.toggle('hidden')
+    const toggleClass =(el,Class) => {
+        el.classList.toggle(Class)
+    }
+
+    toggleClass(btn,'open')
+    toggleClass(nav,'flex')
+    toggleClass(nav,'hidden')
 })
+
+// let myNum = [1,2,3,4,5,6,7];
+
+// myNum.forEach((v) => {
+//     console.log(v*2);
+// });
